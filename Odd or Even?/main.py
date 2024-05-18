d1bc88d655d7d0bf70cef7964c1ee43cd5d85a5c
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-19 02:54:12 +0300

    Add files via upload

---
+++ Odd or Even?/main.py
@@ -0,0 +1,17 @@
+#Checks if a number is odd or even
+
+
+number = int(input("Enter a number\n"))
+
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+
+
+
+
+
+
