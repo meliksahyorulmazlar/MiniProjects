d1193f2fedd6c3f348f7255294047fb556522a34
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-19 12:27:40 +0300

    Update main.py

--- Odd or Even?/main.py
+++ Odd or Even?/main.py
@@ -1,12 +1,12 @@
 #Checks if a number is odd or even
 
-
-number = int(input("Enter a number\n"))
-
-if number % 2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
+if __name__ == "__main__":
+    number:int = int(input("Enter a number\n"))
+    
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
 
 
 
