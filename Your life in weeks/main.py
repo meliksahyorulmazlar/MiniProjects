aaa9e0416a8c551a3c3716113aa57b3059dc4b96
Meliksah Yorulmazlar <yorulk@rpi.edu>
2024-05-22 17:39:46 -0700

    Add files via upload

---
+++ Your life in weeks/main.py
@@ -0,0 +1,17 @@
+#https://waitbutwhy.com/2014/05/life-weeks.html
+
+
+def main():
+    age:int = int(input('How old are you?\n'))
+    print("If we suppose that you will die at the age of 90...")
+    years_left: int = 90 - age
+    days_left:int = years_left*365
+    weeks_left:int = years_left*52
+    print(f"You have {days_left} days left")
+    print(f"You have {weeks_left} weeks left")
+    print(f"You have {years_left} years left")
+
+
+
+if __name__ == "__main__":
+    main()
